6d9f41ffde377f21821e3b3f50ac39fe58626f0c
yuyuyu312 <yusupov.ilgiz2323@gmail.com>
2019-06-13 09:00:56 +0600

    p1task4

---
+++ task5.py
@@ -0,0 +1,17 @@
+
+#Make sure to un-comment the function line below when you are done.
+#Remember to name your function correctly.
+def string_length(x):
+    return len(x)
+string_length("dsdsdsd")
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def test_string_length():
+  assert string_length('hello!') == 6
+  assert string_length('banana') == 6
+  assert string_length('8') == 1
+  assert string_length('funnyguys') == 9
+  assert string_length('101') == 3
+  print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_string_length()
